8714dd8c6564e1499a7b5d07f5d4812cdc482042
sergey_iagfarov <dedaz27@mail.ru>
2022-10-08 17:59:01 +0500

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        // Задание 2
-        int i = 1;
-        while (i <= 10) {
-            System.out.println(i++);
-        }
-        int a = 10;
-        for (;a >= 1;) {
-            System.out.println(a--);
+        // Задание 3
+
+        int population = 12_000_000;
+        int birthRate = 17 * population / 1000;
+        int deathRate = 9 * population / 1000;
+        for (int i = 1; i <= 10; i++) {
+            population = population + birthRate - deathRate;
+            System.out.println("Год " + i + ", численность населения составляет " + population);
         }
     }
 }
+
 
