a9f24126cc6874952be92e7b7c135de4f9b3db1a
sergey_iagfarov <dedaz27@mail.ru>
2022-10-09 14:50:10 +0500

    Задание 5

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        // Задание 4
+        // Задание 5
         int sum = 15000;
         int monthnum = 1;
-        int i = 0;
-        while (sum<=12_000_000) {
-            sum = sum * 1.07;
-            System.out.println("Год " + i + ", сумма накоплений составляет " + sum); i ++;
+        while (sum <= 12_000_000) {
+                sum *= 1.07;
+            if (monthnum % 6 == 0) {
+                System.out.println("Месяц " + monthnum + ", сумма накоплений составляет " + sum);
+            }
+                monthnum++;
+            }
         }
     }
-}
+
 
 
